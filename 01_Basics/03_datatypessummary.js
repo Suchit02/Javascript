20712913c636b338f65c9aa5867153b9e9809241
Suchit kumar mishra <mishrasatya794@gmail.com>
2023-09-14 11:11:04 +0000

    summary datatype

---
+++ 01_Basics/03_datatypessummary.js
@@ -0,0 +1,24 @@
+// Data type =
+
+// 1. Prinitive Datatype
+//     types : Number, Null, Boolean , BigInt, Symbol, String , Undedinde
+// Example of Primtive DataType =
+ const Value = 250;
+ //const LoginAccesss= Null;
+ const UserLogin = true;
+ const BigAmount = BigInt(554) + BigInt(6);
+ const UserName = "Harshita";
+ const UserAutho = undefined;
+
+ console.log(Value,UserAutho,UserLogin,BigAmount,UserName);
+
+ // 2. Refrence (Non-Prinitive )
+ // type: Array , Object , Function 
+ const StudentData=["satvika", true]// array is a collaction of items . we can store primitive data types and Objects and other 
+const Data = {
+     satvik:224,
+     sat :22456
+}
+const User= function (){
+     console.log('i am a function ')
+}
