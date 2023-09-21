bd7b4993cef0bda4cb0912d550802410a6543bcb
Suchit kumar mishra <mishrasatya794@gmail.com>
2023-09-21 10:07:06 +0000

    add objects in list

---
+++ 02_Basics/03_Objects.js
@@ -0,0 +1,18 @@
+// objects is a part of data type 
+// object = we use object for mapning and key value pair and array and function assign 
+
+
+// type of an object is : 1.Singleton   || 2. object litirle 
+
+// Object litirle 
+  const UserData= {
+    name : "Saurabh",
+    'LastName': "Gour",
+    email:'saurabh121@gmail.com',
+    phonenumber:852364,
+    UserLogIn: false,
+    LastLogin:['sunday','monday']
+  }
+
+  console.log(UserData.name);
+  console.log(UserData);
